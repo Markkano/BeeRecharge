095ad5c02bb5698603920802b259d9b81d575333
Marcos Donadio <marcosdonadio95@live.com.ar>
2017-10-07 23:59:29 -0300

    Config Request y Router funcionando

---
+++ Config/Config.php
@@ -0,0 +1,12 @@
+<?php namespace Config;
+define('ROOT', dirname(__DIR__).'/');
+
+## Base de datos
+define('DB_HOST', 'host');
+define('DB_USER', 'user');
+define('DB_PASS', 'pass');
+define('DB_NAME', 'db');
+
+define('IMG_PATH', 'Images/');
+define('MAX_IMG_SIZE', 5000000);
+?>

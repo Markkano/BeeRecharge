22666b46e68bccc8baa4bdc91433c1fed826dd80
Marcos Donadio <marcosdonadio95@live.com.ar>
2017-10-09 21:38:16 -0300

    Agregue credenciales de BD

--- Config/Config.php
+++ Config/Config.php
@@ -2,10 +2,10 @@
 define('ROOT', dirname(__DIR__).'/');
 
 ## Base de datos
-define('DB_HOST', 'host');
-define('DB_USER', 'user');
-define('DB_PASS', 'pass');
-define('DB_NAME', 'db');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '35331374');
+define('DB_NAME', 'BeeRecharge');
 
 define('IMG_PATH', 'Images/');
 define('MAX_IMG_SIZE', 5000000);

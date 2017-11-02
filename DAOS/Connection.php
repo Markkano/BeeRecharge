4d0aef7906a2ff293615b7ef5c54e8d215e6ed86
Marcos Donadio <marcosdonadio95@live.com.ar>
2017-11-02 19:59:09 -0300

    Error Mode: Exception

--- DAOS/Connection.php
+++ DAOS/Connection.php
@@ -12,10 +12,10 @@ class Connection {
   }
 
   private $pdo;
-protected $table = '';
 
   private function __construct() {
     $this->pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
 
   public function Prepare($sql) {
